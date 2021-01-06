795ed25f8e3a130a3c65dc17091a05c479ce1e94
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2021-01-06 00:10:33 -0500

    quarter commit

--- src/public/js/sockets.js
+++ src/public/js/sockets.js
@@ -9,6 +9,12 @@ module.exports = (io) => {
         Ok: true,
         nickName: socket.nickname,
       });
+      //Se escucha el evento emitido userCoordinates. 
+      socket.on('userCoordinates', (coords) => {
+        console.log('userCoordinates: ', coords);
+        //Se transmite y emite a todos los usuarios conectados
+        socket.broadcast.emit('newUserCordinates', coords);
+    });
     });
     socket.on("disconnect", () => {
       console.log("Disconnect only ...");
